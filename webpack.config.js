4a2d3e327d4f14d1c5c7fac52a94135dd1b82241
Willin Wang <willin@willin.org>
2018-07-03 17:59:30 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    entry: './style.scss',
+    output: {
+      // This is necessary for webpack to compile
+      // But we never use style-bundle.js
+      filename: 'style-bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css'
+            }
+          },
+          'extract-loader',
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./node_modules']
+            }
+          }
+        ]
+      }]
+    }
+  },
+  {
+    entry: './app.js',
+    output: {
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        use: 'babel-loader'
+      }]
+    }
+  }
+];
